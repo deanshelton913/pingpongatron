63b0f1284d15165cebf8fac17f60086cd69f563b
Dean Shelton at home <deanshelton913@gmail.com>
2014-10-04 23:47:49 -0700

    invalidate s3 tokens, and use env vars

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,9 +39,9 @@ Rails.application.configure do
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => 'pingpongotron',
-    :access_key_id => 'AKIAIVGZMNFWIEEOAB2A',
-    :secret_access_key => '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'
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
 end
